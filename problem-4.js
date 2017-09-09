dd2a9033ccc1f2c357b20a00528ded5172d7d8c5
Ian Gregor <iangregor@gregorwebdesign.net.au>
2017-09-09 16:39:14 +1000

    factor lambdas out to be passed in as params

--- problem-4.js
+++ problem-4.js
@@ -1,92 +1,105 @@
 let euler4 = (
-    (pair) => (fst) => (snd) => 
-    (    
-        // bootstrap our generic iterator function to allow recursion
-        ( (f) => (fp) => (fi) => (i) => f(f) (fp)(fi) (i) )
-        // generic iterator without accumulator
-        // fp is our predicate to end the loop, fi increments the i value. i is our counter and output
+    (fix) => (pipe) => (pair) => (fst) => (snd) => (not) =>
+    (
+        (pipeline) => (intToList) => (reverse) => (listsEqual) => (copyF) =>
         (
-            (f) => (fp) => (fi) => (i) => 
-            fp(i) ? f(f) (fp)(fi) (fi(i)) : i
-        )
-        /* fp: false if n*m is palindromic */ 
-        (
-            (i) => (
-                ((f0) => (f1) => (f2) => (f3) => (x) => f0(f1(f2(f3(x)))))
-                // f0: not
-                ((x) => !x)
-                // f1: arrays equal?
-                (
-                    // roll fp, fi into f because it's short.
-                    ((f) => (ns) => f(f) (ns))
-                    (
-                        (f) => (ns) =>
-                        (
-                            (xs) => (ys) => 
-                            xs === undefined && ys === undefined ? true :
-                            snd(xs) === snd(ys) ? f(f) (pair(fst(xs))(fst(ys))) : 
-                            false
-                        )
-                        (fst(ns))
-                        (snd(ns))
-                    )
-                )
-                // f2: reverse a copy and provide both
-                (
-                    ((f) => (x) => pair(x)(f(x)))
-                    (
-                        // reverse: just pop a stack onto another stack until empty
-                        ((f) => (acc) => (xs) => f(f) (xs) (acc))
-                        (
-                            (f) => (xs) => (acc) => 
-                            xs !== undefined ? 
-                                f(f) (fst(xs)) ((pair)(acc)(snd(xs))) : 
-                                acc
-                        )
-                        // undefined is our empty list value
-                        (undefined)
-                    )
-                )
-                // f3: int to array by push mod 10, divint 10, repeat.
-                (
-                    // Swap (acc) and (n) to let f3 be point-free
-                    // e.g., point-free: f vs non: (x) => f(x)
-                    ((f) => (acc) => (n) => f(f) (n) (acc))
-                    (
-                        (f) => (n) => (acc) => 
-                        n > 0 ? f(f) (n/10|0) (pair(acc)(n % 10)) : acc
-                    )
-                    (undefined)
-                )
-                // x
-                (fst(i)*snd(i))
+            (notPalindromic) => (walkDown) =>
+            // generic iterator without accumulator
+            // fp is our predicate to end the loop, fi increments the i value. i is our counter and output
+            fix (
+                (f) => (fp) => (fi) => (i) => 
+                fp(i) ? f(f) (fp)(fi) (fi(i)) : fst(i)*snd(i)
             )
+            /* fp: */ 
+            (notPalindromic)
+            /* fi */ 
+            (walkDown(1000))
+            /*  i */ 
+            (pair(999)(999))
         )
-        /* fi */ 
+        /* notPalindromic */
         (
-            (
-                // walk down from 999*999 through the factors in a breadth first motion.
-                // This is a naive walk as it will do both 999*998 and 998*999
-                (u) => (i) => 
-                snd(i) + 1 > u ? 
-                    pair(u) (fst(i)-1) : 
-                    pair(fst(i)-1) (snd(i)+1)
-            )
-            (1000)
+            pipeline(5)
+                ((i) => fst(i)*snd(i))
+                (intToList)    
+                (copyF(reverse))                    
+                (listsEqual)
+                (not)    
+                
         )
-        /*  i */ 
-        (pair(999)(999))
+        /* walk down */
+        (
+            // walk down from 999*999 through the factors in a breadth first motion.
+            // This is a naive walk as it will do both 999*998 and 998*999
+            (u) => (i) => 
+            snd(i) + 1 > u ? 
+                pair(u) (fst(i)-1) : 
+                pair(fst(i)-1) (snd(i)+1)
+        )
     )
+    /* composer: takes n functions and returns them composed */
+    (
+        fix(
+            (f) => (n) => (g) => 
+                n > 1 ? 
+                (h) => ( f(f) (n-1) (pipe(g)(h)) ) : 
+                (x) => (g(x)) 
+        )
+    )
+    // intToList.
+    (
+        fix (
+            (f) => (acc) => (n) => 
+            n > 0 ? f(f) (pair(acc)(n % 10)) (n/10|0) : acc
+        )
+        (undefined)
+    )
+    // reverse: just pop a stack onto another stack until empty
+    (
+        fix (
+            (f) => (acc) => (xs) => 
+            xs !== undefined ? 
+                f(f) ((pair)(acc)(snd(xs))) (fst(xs)) : 
+                acc
+        )
+        // undefined is our empty list value
+        (undefined)
+    )
+    // listsEqual: compares elements in two flat lists.
+    (
+        // roll fp, fi into f because it's short.
+        fix (
+            (f) => (ns) =>
+            (
+                (xs) => (ys) => 
+                xs === undefined && ys === undefined ? true :
+                snd(xs) === snd(ys) ? f(f) (pair(fst(xs))(fst(ys))) : 
+                false
+            )
+            (fst(ns)) //xs
+            (snd(ns)) //ys
+        )
+    )
+    // copyF: 
+    ((f) => (x) => pair(x)(f(x)))
 )
+// You don't need to explicitly list every param to bootstrap recursion... 
+// you can just "fix" the function and the currying will take care of it.
+/* fix */
+((f) => f(f) )
+/* pipe: */
+((f) => (g) => (x) => g(f(x)))
 // I felt that using arrays and array destructuring was kinda cheating...
 // so here are the building blocks of lists and trees.
 // Using undefined as {} or "empty list"
-// pair: can create a list or tree. E.g., [[[{},1],2],3] is [1,2,3]
+/* pair: can create a list or tree. E.g., [[[{},1],2],3] is [1,2,3] */
 ((x) => (y) => (f) => (f)(x)(y) )
-// first: get the first element of the tuple where the rest of the array is stored
+/* first: get the first element of the tuple where the rest of the array is stored */
 ((p) => p((x) => (y) => x) )
-// second: get the second element where the value is stored
+/* second: get the second element where the value is stored */
 ((p) => p((x) => (y) => y) )
+/* not */
+((x) => !x)
 
 
 
